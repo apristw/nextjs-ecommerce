035e124cfc615f507d4fb98d2406fa9e46d270d6
apristw <mailapriawan@gmail.com>
2024-10-16 15:48:27 +0700

    Add Feature | Create hero section with slider effect

--- src/components/NavIcons.tsx
+++ src/components/NavIcons.tsx
@@ -33,7 +33,7 @@ const NavIcons = () => {
         onClick={handleProfile}
       />
       {isProfileOpen && (
-        <div className="absolute p-4 rounded-md top-12 left-0 text-sm shadow z-20">
+        <div className="absolute p-4 bg-white rounded-md top-12 left-0 text-sm shadow z-20">
           <Link href="/">Profile</Link>
           <div className="mt-2 cursor-pointer">Logout</div>
         </div>
